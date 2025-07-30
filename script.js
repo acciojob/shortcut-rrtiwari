dcc63d3aaeaedef5cfad3f6bdcea00acea8d0d05
Rahul Tiwari <128848653+rrtiwari@users.noreply.github.com>
2025-07-30 11:20:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function shortcut(s1, s2) {
    return (s);
 }
 
-// Do not change the code below.
-// const s1 = prompt("Enter s1:");
-// const s2 = prompt("Enter s2:");
+//Do not change the code below.
+const s1 = prompt("Enter s1:");
+const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
